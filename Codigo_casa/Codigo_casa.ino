29fa6c809b8c9dfab668304840ce56ef83e5fa8e
Osmar Bruno <brunoosmar442@gmail.com>
2022-10-04 11:26:34 -0300

    Adicionando ultrassonico
    Adicionando ultrassonico e removendo led do banheiro

--- Codigo_casa/Codigo_casa.ino
+++ Codigo_casa/Codigo_casa.ino
@@ -1,20 +1,28 @@
 //#include "BluetoothSerial.h"
 #include <SoftwareSerial.h>
 
+
+#define PinoTrigger 6
+#define PinoEcho 7
+int duracao = 0;
+int distanciaSensor = 0;
+unsigned long tempoUltrasonico = 0;
+
 #define ligaledCozinha 'a'
 #define ligaledSala 'b'
 #define ligaledQuarto 'c'
-#define ligaledBanheiro 'd'
 
 #define pinoCozinha 12
 #define pinoSala 13
 #define pinoQuarto 14
-#define pinoBanheiro 27
+#define pinoLedUltrassonico 27
                                                                                                              
 bool cozinhaLigado = false;
 bool salaLigado = false;
 bool quartoLigado = false;
-bool banheiroLigado = false;
+
+void ligaLedUltrassonico(int distancia);
+int distanciaLigaLed = 20; //centímetros
 
 //BluetoothSerial SerialBT;
 SoftwareSerial SerialBT(10, 11); // RX, TX do Arduino
@@ -27,10 +35,26 @@ void setup() {
   pinMode(pinoCozinha, OUTPUT);
   pinMode(pinoSala, OUTPUT);
   pinMode(pinoQuarto, OUTPUT);
-  pinMode(pinoBanheiro, OUTPUT);
+  pinMode(pinoLedUltrassonico, OUTPUT);
+  pinMode(PinoEcho,INPUT);
+  pinMode(PinoTrigger,OUTPUT);
   
 }
 void loop() {
+  //Acionamento do Trigger
+  digitalWrite(PinoTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PinoTrigger,LOW);
+  
+
+  //Leitura e armazenamento do Trigger
+  duracao = pulseIn(PinoEcho, HIGH);
+  distanciaSensor = duracao*0.017175;
+  //Serial.print(distancia);
+  //Serial.println("cm");  
+
+  ligaLedUltrassonico(distanciaSensor);
+  
   //Caso meu monitor serial receba algo ele envia essses pacotes para o bluetooth
   if(Serial.available()){
     SerialBT.write(Serial.read());
@@ -57,13 +81,19 @@ void loop() {
       quartoLigado = !quartoLigado;
       digitalWrite (pinoQuarto, quartoLigado);  
     }
-  
-    //BANHEIRO
-    if(SerialBT.read() == ligaledBanheiro){
-      banheiroLigado = !banheiroLigado;
-      digitalWrite (pinoBanheiro, banheiroLigado);  
-    }
   }
   
-  delay(20);
- }
+  delay(10);
+}
+
+void ligaLedUltrassonico(int distancia)
+{
+  //Caso a distancia que o ultrassonico esteja marcando
+  //for menor que a distancia pré-definida ele aciona o led
+  if(distancia <= distanciaLigaLed)
+  {
+    digitalWrite(pinoLedUltrassonico, HIGH);
+  }else{
+    digitalWrite(pinoLedUltrassonico, LOW);
+  }
+}
