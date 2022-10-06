2d811866285a9e24d8bd921df1aa9ac20e2d4af5
Osmar Bruno <brunoosmar442@gmail.com>
2022-10-06 09:08:52 -0300

    Fixing bugs and improving the code

--- Codigo_casa/Codigo_casa.ino
+++ Codigo_casa/Codigo_casa.ino
@@ -1,10 +1,11 @@
 #include "BluetoothSerial.h"
 
-#define PinoTrigger 6
-#define PinoEcho 7
+//Ultrassonico
+#define PinoTrigger 34
+#define PinoEcho 35
 int duracao = 0;
 int distanciaSensor = 0;
-unsigned long tempoUltrasonico = 0;
+int distanciaLigaLed = 20; //centímetros
 
 #define ligaledCozinha 'a'
 #define ligaledSala 'b'
@@ -13,28 +14,83 @@ unsigned long tempoUltrasonico = 0;
 #define pinoCozinha 12
 #define pinoSala 13
 #define pinoQuarto 14
-#define pinoLedUltrassonico 27
-//void ligaLedBT();
+#define ledUltrassonico 27
                                                                                                              
 bool cozinhaLigado = false;
 bool salaLigado = false;
 bool quartoLigado = false;
 
-//void ligaLedUltrassonico(int distancia);
-int distanciaLigaLed = 20; //centímetros
 
 BluetoothSerial SerialBT;
+void serialRead();
+void ligaLedBT();
+void ligaLedUltrassonico();
 
 
-void ligaLedUltrassonico(int distancia)
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);  
+  Serial.println("setup");
+  SerialBT.begin("casaTCC");
+  
+  
+  pinMode(pinoCozinha, OUTPUT);
+  pinMode(pinoSala, OUTPUT);
+  pinMode(pinoQuarto, OUTPUT);
+
+  //O código buga nesta parte
+  pinMode(ledUltrassonico, OUTPUT);
+  pinMode(PinoEcho,INPUT);
+  pinMode(PinoTrigger,OUTPUT);
+}
+
+void loop() {
+  /*Serial.println("loop");
+  digitalWrite(14, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);             // wait for a second
+  digitalWrite(14, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); */ 
+  Serial.print("loop");
+  ligaLedUltrassonico();
+  serialRead();
+}
+
+void serialRead()
 {
+  //Caso o monitor serial receba algo ele envia esses pacotes para o bluetooth
+  if(Serial.available()){
+    SerialBT.write(Serial.read());
+  }
+
+  //Caso receba algo do bluetooth, leia o que foi recebido
+  if(SerialBT.available()) {
+    Serial.println(SerialBT.read() + "\n\n");
+    ligaLedBT();
+  }
+}
+
+void ligaLedUltrassonico()
+{ 
+  //Da varios pulsos no pino trigger
+  digitalWrite(PinoTrigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PinoTrigger,LOW);
+
+
+  //Leitura e armazenamento do Trigger
+  duracao = pulseIn(PinoEcho, HIGH);
+  distanciaSensor = duracao*0.017175;
+  Serial.print(distanciaSensor);
+  Serial.println("cm");  
+
+ 
   //Caso a distancia que o ultrassonico esteja marcando
   //for menor que a distancia pré-definida ele aciona o led
-  if(distancia <= distanciaLigaLed)
+  if(distanciaSensor >= distanciaLigaLed)
   {
-    digitalWrite(pinoLedUltrassonico, HIGH);
+    digitalWrite(ledUltrassonico, HIGH);
   }else{
-    digitalWrite(pinoLedUltrassonico, LOW);
+    digitalWrite(ledUltrassonico, LOW);
   }
 }
 
@@ -56,65 +112,5 @@ void ligaLedBT()
   if(SerialBT.read() == ligaledQuarto){
     quartoLigado = !quartoLigado;
     digitalWrite (pinoQuarto, quartoLigado);  
-  }
-}
-
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(115200);
-  Serial.println("setup");
-  SerialBT.begin("casaTCC");
-  // initialize digital pin LED_BUILTIN as an output.
-
-  
-  
-  pinMode(pinoCozinha, OUTPUT);
-  pinMode(pinoSala, OUTPUT);
-  pinMode(pinoQuarto, OUTPUT);//led
-
-  //O código buga nesta parte
-  //pinoLedUltrassonico, OUTPUT);
-  //pinMode(PinoEcho,INPUT);
-  //pinMode(PinoTrigger,OUTPUT);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  /*Serial.println("loop");
-  digitalWrite(14, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);             // wait for a second
-  digitalWrite(14, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000); */    
-
-
-  //Caso meu monitor serial receba algo ele envia essses pacotes para o bluetooth
-  if(Serial.available()){
-    SerialBT.write(Serial.read());
-    Serial.println("SERIAL\n");
-  }
-
-  //Caso eu receba algo do bluetooth eu vou ler o que foi recebido
-  if(SerialBT.available()) {
-    Serial.println(SerialBT.read());
-    //ligaLedBT(SerialBT.read());
-
-    //COZINHA
-    if(SerialBT.read() == 'a'){
-      cozinhaLigado = !cozinhaLigado;
-      digitalWrite (pinoCozinha, cozinhaLigado);  
-    }
-    
-    //SALA
-    if(SerialBT.read() == 'b'){
-      salaLigado = !salaLigado;
-      digitalWrite (pinoSala, salaLigado);  
-    }
-    
-    //QUARTO
-    if(SerialBT.read() == 'c'){
-      quartoLigado = !quartoLigado;
-      digitalWrite (pinoQuarto, quartoLigado);  
-    }
   }
 }
